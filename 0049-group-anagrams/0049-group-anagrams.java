39d0b3575fc8deabc4315a75b8f77c16e00f0638
Anushka Palewar <anushkapalewar81@gmail.com>
2025-12-26 11:54:51 +0530

    Time: 7 ms (74.1%), Space: 49.7 MB (42.39%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> mp=new HashMap<>();
+        for(String str: strs){
+            char arr[]=str.toCharArray();
+            Arrays.sort(arr);
+            String key=new String(arr);
+
+            mp.putIfAbsent(key,new ArrayList<>());
+            mp.get(key).add(str);
+        }
+        return new ArrayList<>(mp.values());
+    }
+}
