d293794f7273d7fcd84618fcba0fd39921f4d55a
Anushka Palewar <anushkapalewar81@gmail.com>
2026-03-20 09:20:33 +0530

    Time: 5 ms (66.35%), Space: 47.2 MB (38.01%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision-03-20-2026-09-20-33.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st=new Stack<>();
+
+        for(int num:asteroids){
+            if(num>0){
+                st.push(num);
+            }else{
+                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(num)){
+                    st.pop();
+                }
+
+                if(!st.isEmpty() && st.peek()==Math.abs(num)){
+                    st.pop();
+                }else if(st.isEmpty() || st.peek()<0){
+                    st.push(num);
+                }
+            }
+        }
+
+        int res[]=new int[st.size()];
+        for(int i=st.size()-1;i>=0;i--){
+            res[i]=st.pop();
+        }
+
+        return res;
+    }
+}
