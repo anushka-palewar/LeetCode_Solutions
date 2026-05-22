f565209e3a4cc42a3fa4146b819917a989ac57cf
Anushka Palewar <anushkapalewar81@gmail.com>
2026-05-22 12:50:31 +0530

    Time: 0 ms (100%), Space: 42.7 MB (73.09%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber-05-22-2026-12-50-46.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(nums,dp,0);
+    }
+
+    static int helper(int nums[],int dp[],int i){
+        if(i>=nums.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+
+        int pick=nums[i]+helper(nums,dp,i+2);
+        int notPick=helper(nums,dp,i+1);
+        return dp[i]=Math.max(pick,notPick);
+    }
+}
