77643f8ad81235f7f05ced4099062c782fa8f991
Anushka Palewar <anushkapalewar81@gmail.com>
2026-07-21 07:06:46 +0530

    Time: 20 ms (84.15%), Space: 56.5 MB (56.96%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int i=0,j=people.length-1,count=0;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+            }
+            j--;
+            count++;
+        }
+        return count++;
+    }
+}
