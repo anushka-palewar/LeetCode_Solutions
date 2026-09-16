07abdea4bbd552113781bb4c31df4c53156a988d
Anushka Palewar <anushkapalewar81@gmail.com>
2026-09-16 21:56:53 +0530

    Time: 1 ms (99.22%), Space: 43.2 MB (86.01%) - LeetHub

---
+++ 0139-word-break/0139-word-break-09-16-2026-21-56-51.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set=new HashSet<>(wordDict);
+        int n=s.length();
+        
+        int maxLen=0;
+        for(String str:set){
+            maxLen=Math.max(maxLen,str.length());
+        }
+
+        boolean dp[]=new boolean[n+1];
+        dp[0]=true;
+
+        for(int i=1;i<=n;i++){
+            for(int j=i-1;j>=Math.max(0,i-maxLen);j--){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
